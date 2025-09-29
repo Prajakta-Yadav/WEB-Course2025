d40aa935aefaa725cc27966cb34248473302e129
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-09-29 18:08:05 +0530

    Add files via upload

---
+++ Javascript/Operators/RelationalOperators.js
@@ -0,0 +1,40 @@
+let a =100;
+let b = 22;
+
+result = a > b;
+console.log("a is greater than b: " + result);
+console.log("a: " + a);
+console.log("b: " + b);
+
+result = a < b;
+console.log("a is less than b: " + result);
+console.log("a: " + a);
+console.log("b: " + b);
+
+result = a == b;
+console.log("a is equal to b: " + result);
+console.log("a: " + a);
+console.log("b: " + b);
+
+
+result = a === b;
+console.log("a is equal equal to b: " + result);
+console.log(typeof(a));
+console.log(typeof(b));
+console.log("a: " + a);
+console.log("b: " + b);
+
+
+result = a != b;
+console.log("a is not equal to b: " + result);
+console.log("a: " + a);
+console.log("b: " + b);
+
+let c = 100;
+let d = 100;
+result = c === d;
+console.log("c is equal equal to d: " + result);
+console.log(typeof(c));
+console.log(typeof(d));
+console.log("c: " + c);
+console.log("d: " + d);
