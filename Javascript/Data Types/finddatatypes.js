0d47d01622451dd3a3d5b984e8f0a73fad3bc0ec
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-09-29 18:06:16 +0530

    Create finddatatypes.js

---
+++ Javascript/Data Types/finddatatypes.js
@@ -0,0 +1,31 @@
+//Q. 1 Declare a variable with a number value and print its type using typeof(). 
+
+let num = [10,20,30,40,50];
+console.log(num);
+console.log(typeof(num));
+
+let name = "Prajakta";
+console.log(name);
+console.log(typeof(name));
+
+let num1 = 100;
+console.log(num1);
+console.log(typeof(num1));
+
+//Q. 2 Concatenate a string and a number and observe the output.
+
+let LastName = "Yadav";
+console.log(name + " " + LastName);
+console.log(typeof(name));
+
+//Q. 3 Create a boolean variable, change its value, and pint it. 
+
+isStudent = true;
+console.log(isStudent);
+
+isStudent = false;
+console.log(isStudent);
+
+itisTrue = true;
+console.log(itisTrue);
+console.log(typeof(itisTrue));
