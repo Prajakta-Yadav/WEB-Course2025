a7ba242321f5f4fa41089fd02b44c4ad1146bffb
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-09-29 18:07:41 +0530

    Create ArithmeticOperators.js

---
+++ Javascript/Operators/ArithmeticOperators.js
@@ -0,0 +1,20 @@
+let a =100;
+let b = 22;
+
+result = a + b;
+console.log("Addition: " + result);
+
+result = a - b;
+console.log("Subtraction:" + result);
+
+result = a * b;
+console.log("Multiplication: " + result);
+
+result = a / b;
+console.log("Division: " + result);
+
+result = a % b;
+console.log("Module: " + result);
+
+result = a ** b;
+console.log("Power: " + result);
