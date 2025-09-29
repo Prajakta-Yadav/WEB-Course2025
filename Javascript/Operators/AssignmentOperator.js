05a67a52214d9ed3a2b1cd80fb82e7d6370159f4
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-09-29 18:57:27 +0530

    Add files via upload

---
+++ Javascript/Operators/AssignmentOperator.js
@@ -0,0 +1,20 @@
+let x = 10;
+console.log(x); //10
+
+x += 5;
+console.log(x); //15
+
+x -= 3;
+console.log(x); //12
+
+x *= 5;
+console.log(x); //60
+
+x /= 4;
+console.log(x); //15
+
+x %= 2;
+console.log(x); //1
+
+x *= 3; 
+console.log(x); //3 
---
+++ Javascript/Operators/ComparisonOperator.js
@@ -0,0 +1,54 @@
+let num1 = true;
+let num2 = '1';
+
+console.log(typeof(num1));
+console.log(typeof(num2));
+
+if(num1 == num2){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
+// case 2
+if(num1 === num2){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
+// case 3 
+let num3 = false;
+let num4 = '0';
+
+
+if(num3 == num4){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
+if(num3 === num4){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
+
+//case 4
+
+let num5 = false;
+let num6 = '1';
+
+
+if(num5 == num6){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
