babafeca186178a838fad97c34814194478fe832
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-10-04 22:17:22 +0530

    Update SquareNum.js

--- Javascript/function/SquareNum.js
+++ Javascript/function/SquareNum.js
@@ -8,4 +8,29 @@ console.log(square(20));
 let squarenum = (num) => num*num;
 
 console.log(squarenum(30));
-squarenum(40);  //with parameter function need to write in console statement. but without parameter or default function does not need console statement. we can directly call it. 
+squarenum(40); // no output
+
+//IIFE => Immediately Invoked Function Expression
+(function(num){
+    console.log(num*num);
+})(50);
+
+//IIFE with return
+let sqr = (function(num){
+    return num*num;
+})(60);
+
+console.log(sqr);
+
+//IIFE with return and arrow function
+console.log(((num) => num*num)(70)); 
+
+let sqr1 = ((num) => num*num)(80);
+console.log(sqr1);  
+
+//function name = argument    condition 
+let squarenum1 = num => num*num;    
+console.log(squarenum1(90));
+
+squarenum1(100); // no output
+console.log(squarenum1(100)); //output
