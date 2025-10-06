748516109026e7cac62bff29dbf85a53c557ef25
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-10-06 13:02:15 +0530

    Create dowhile.js

---
+++ Javascript/Loops/dowhile.js
@@ -0,0 +1,15 @@
+// prints number 1 to 5
+
+console.log("Print 1 ro 5 number");
+let i =1;
+do{
+    console.log(i);
+    i++;
+}while(i<=5)
+
+//print welcome at least once regardless of the condition 
+
+do{
+    console.log("Welcome");
+    i++;
+} while(i>=10)
