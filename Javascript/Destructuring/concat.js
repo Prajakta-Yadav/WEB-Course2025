006b4618d6822129a97865c3a666510d7f3e9947
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-10-09 21:21:22 +0530

    Add files via upload

---
+++ Javascript/Destructuring/concat.js
@@ -0,0 +1,8 @@
+//Spread Operator ...
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+let moreFruits = ["Strawberry", "Pineapple", "Mango"];
+
+let all = [...fruits,...moreFruits];
+console.log(all);
---
+++ Javascript/Destructuring/spreadOperator.js
@@ -0,0 +1,6 @@
+// ...
+
+let arr = [10, 30, 50, 70];
+let arr1 = [...arr];
+console.log(arr);
+console.log(arr1);
