0a67318a7677f7afbe4d20f5669d1677ff152189
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-10-09 21:03:16 +0530

    Create AddDelete.js

---
+++ Javascript/Set/AddDelete.js
@@ -0,0 +1,14 @@
+let numbers = new Set([1,2,3,4,5]);
+
+//add() method is used to add elements to a Set object
+numbers.add(100)
+numbers.add(200)
+numbers.add(1)
+numbers.add(78)
+numbers.add(78)
+console.log(numbers);
+
+//delete() method is used to remove an element from a Set object
+numbers.delete(100);
+console.log(numbers);
+
